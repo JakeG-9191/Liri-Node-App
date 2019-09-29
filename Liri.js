8a9df25feb3269c099c40e4430eeca8d12cbd996
Jake Garlick <jacobgarlick@msn.com>
2019-09-29 14:23:38 -0600

    added functionality for movie info to be easier to read by user

--- Liri.js
+++ Liri.js
@@ -19,7 +19,15 @@ var concertInfo = "concert-this";
 
 if (userRequest === movieInfo) {
     axios.get("http://www.omdbapi.com/?t=" + userInput + "&plot=short&apikey=trilogy").then(function(response) {
-        console.log(response.data);
+        console.log("-----------------Movie Info---------------")
+        console.log("Title: " + response.data.Title)
+        console.log("Release Year: " + response.data.Year)
+        console.log("IMDB Rating: " + response.data.Ratings[0].Value)
+        console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value)
+        console.log("Country of Production: " + response.data.Country)
+        console.log("Languages: " + response.data.Language)
+        console.log("Plot: " + response.data.Plot)
+        console.log("Actors: " + response.data.Actors)
         }).catch(function(error) {
         if (error.response) {
             console.log(error.response.data);
